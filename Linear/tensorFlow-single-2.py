8b5fc40b11975d41d0f1cc87ddb027b9ac91bbae
flyingseal66 <2576209723@qq.com>
2018-10-28 12:44:40 +0000

    add files

--- Linear/tensorFlow-single-2.py
+++ Linear/tensorFlow-single-2.py
@@ -1,13 +1,7 @@
-# 用TensorFlow实现线性回归算法
 #----------------------------------
-#
 # This function shows how to use TensorFlow to
 # solve linear regression.
 # y = Ax + b
-#
-# We will use the iris data, specifically:
-#  y = Sepal Length
-#  x = Petal Width
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -29,43 +23,44 @@ sess = tf.Session()
 # Load the data
 # iris.data = [(Sepal Length, Sepal Width, Petal Length, Petal Width)]
 iris = datasets.load_iris()
-x_vals = np.array([x[3] for x in iris.data])
+x_vals = np.array([x[2] for x in iris.data])
+x_test = np.append(np.append(x_vals[30:40], x_vals[70:80]), x_vals[110:120])
+x_vals = np.append(np.append(np.append(x_vals[0:30], x_vals[40:70]), x_vals[80:110]), x_vals[120: 150])
 x_vals = normaliseData(x_vals)
-y_vals = np.array([y[0] for y in iris.data])
+
+y_vals = np.array([y[3] for y in iris.data])
+yt_est = np.append(np.append(y_vals[30:40], y_vals[70:80]), y_vals[110:120])
+y_vals = np.append(np.append(np.append(y_vals[0:30], y_vals[40:70]), y_vals[80:110]), y_vals[120: 150])
 y_vals = normaliseData(y_vals)
 
 print(x_vals.shape)
 print(y_vals.shape)
-x_test = x_vals[0:30]
-y_test = y_vals[0:30]
 
-# 批量大小
 batch_size = 25
 
-# Initialize 占位符
+# Initialize
 x_data = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
-# 模型变量
+# variavles
 A = tf.Variable(tf.random_normal(shape=[1,1]))
 b = tf.Variable(tf.random_normal(shape=[1,1]))
 
-# 增加线性模型，y=Ax+b
+# model，y=Ax+b
 model_output = tf.add(tf.matmul(x_data, A), b)
 
-# 声明L2损失函数，其为批量损失的平均值。
+# Loss of L2
 loss = tf.reduce_mean(tf.square(y_target - model_output))
 
-# 声明优化器 学习率设为0.05
+# learning rate
 my_opt = tf.train.GradientDescentOptimizer(0.05)
 train_step = my_opt.minimize(loss)
 
-# 初始化变量
+# init
 init = tf.global_variables_initializer()
 sess.run(init)
 
-# 批量训练遍历迭代
-# 迭代100次，每25次迭代输出变量值和损失值
+# iteration
 loss_vec = []
 for i in range(1000):
     rand_index = np.random.choice(len(x_vals), size=batch_size)
@@ -74,49 +69,41 @@ for i in range(1000):
     sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
     temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
     loss_vec.append(temp_loss)
-    if (i+1)%100==0:
+    if (i+1)%20==0:
         print('Step #' + str(i+1) + ' A = ' + str(sess.run(A)) + ' b = ' + str(sess.run(b)))
         print('Loss = ' + str(temp_loss))
 
-# 抽取系数
+# Get A and b
 [slope] = sess.run(A)
 [y_intercept] = sess.run(b)
 
-# 创建最佳拟合直线
+# best_fit line
 best_fit = []
 for i in x_vals:
   best_fit.append(slope*i+y_intercept)
 
-# 绘制两幅图
-# 拟合的直线
+#fit line
 plt.plot(x_vals, y_vals, 'o', label='Data Points')
 plt.plot(x_vals, best_fit, 'r-', label='Best fit line', linewidth=3)
 plt.legend(loc='upper left')
-plt.title('Sepal Length vs Pedal Width')
-plt.xlabel('Pedal Width')
-plt.ylabel('Sepal Length')
+plt.title('Petal Length and Petal Width (TensorFlow)')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
 plt.show()
 
 # Plot loss over time
-# 迭代100次的L2正则损失函数
 plt.plot(loss_vec, 'k-')
-plt.title('L2 Loss per Generation')
+plt.title('L2 Loss per Generation (TensorFlow)')
 plt.xlabel('Generation')
 plt.ylabel('L2 Loss')
 plt.show()
 
-#Prediction Error
+# Prediction Error
 error = y_vals - best_fit
 plt.hist(error, bins=10)
 plt.xlabel("Prediction Error [1000$]")
 plt.show()
 _ = plt.ylabel("Count")
 
+print("Mean squared error: %.5f" % mean_squared_error(y_vals, best_fit))
 print('Variance score: %.5f' % r2_score(y_vals, best_fit))
-
-
-# plt.plot(x_test, y_test, 'bo', label='Testing data')
-# # x_vals = np.array(x_vals)
-# # plt.plot(x_vals, x_vals*sess.run(A) + sess.run(b), label='Fitted line')
-# # plt.legend()
-# # plt.show()
